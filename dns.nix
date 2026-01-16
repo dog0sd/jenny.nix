938ee9077cbf9c84877acfda68b2eea423e4fa86
dog0sd <dog0_sd@pm.me>
2026-01-16 02:27:11 +0100

    add DoH

---
+++ dns.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+      server_names = [ "cloudflare" "google" "adguard-dns" ];
+      doh_servers = true;
+      require_dnssec = false;
+      require_nolog = true;
+      require_nofilter = true;
+      force_tcp = true;
+    };
+  };
+
+  services.resolved.enable = false;
+
+  networking.nameservers = [ "127.0.0.1" "::1" ];
+  
+  networking.networkmanager.dns = "none";
+}
